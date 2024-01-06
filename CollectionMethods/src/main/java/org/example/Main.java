27a86c5f6b38ac4c1354333cc8f64375a7545254
kshitizgupta108@gmail.com <kshitizgupta108@gmail.com>
2024-01-06 15:23:23 +0530

    collections methods

--- CollectionMethods/src/main/java/org/example/Main.java
+++ CollectionMethods/src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -19,7 +16,6 @@ public class Main {
 
         List<Card> cards = new ArrayList<>(52);
         Collections.fill(cards,aceOfCard );
-        Card.printDeck(cards, "Ace of Hearts",1);
         System.out.println(cards.size());
 
         List<Card> aceofHearts = Collections.nCopies(13, aceOfCard);
@@ -28,6 +24,27 @@ public class Main {
         List<Card> temp = new ArrayList<>();
         temp = List.copyOf(aceofHearts);
         Card.printDeck(temp, "copy", 1);
-        
+
+        Collections.shuffle(deck);
+        Card.printDeck(deck,"suffled", 4);
+
+        Collections.reverse(deck);
+        Card.printDeck(deck, "revered",4);
+
+        Comparator<Card> sortingAlgorithm = Comparator.comparing((Card card)->card.rank).thenComparing((Card card)->card.suit);
+
+        deck.sort( sortingAlgorithm);
+        Card.printDeck(deck, "soretd deck", 13);
+
+        int ans = Collections.binarySearch(deck, Card.getFaceCard(Card.Suit.CLUB,'K'), sortingAlgorithm);
+        System.out.println(deck.get(ans));
+        System.out.println(Collections.frequency(deck,deck.get(0)));
+        Card.printDeck(deck);
+        System.out.println(Collections.min(deck, sortingAlgorithm));
+        System.out.println(Collections.max(deck, sortingAlgorithm));
+
+
+        Collections.rotate(deck, 2);
+        Card.printDeck(deck);
     }
 }
